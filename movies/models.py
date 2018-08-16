a10945cd43b6e392071f75aa08970da7fc912ec1
devdob <a.nassif012@gmail.com>
2018-08-17 01:51:44 +0300

    add __str__ to classes

--- movies/models.py
+++ movies/models.py
@@ -12,13 +12,17 @@ class Movie(models.Model):
     movie_uid = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.movie_uid
+
 
 class MovieCharacter(models.Model):
     """
     Movie character model
-
-    p.s when data is small
     """
     character_uid = models.CharField(max_length=255)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.character_uid
